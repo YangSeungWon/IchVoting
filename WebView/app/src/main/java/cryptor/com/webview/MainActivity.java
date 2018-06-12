2a290cfd6606a4ef1ef9566db197566dcfef8ea2
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2018-06-13 03:45:52 +0900

    add

--- WebView/app/src/main/java/cryptor/com/webview/MainActivity.java
+++ WebView/app/src/main/java/cryptor/com/webview/MainActivity.java
@@ -2,23 +2,74 @@ package cryptor.com.webview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
-    private WebView mwebView;
-
+    private WebView mWebView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mwebView.getSettings().setJavaScriptEnabled(true);
-        mwebView.getSettings().setDomStorageEnabled(true);
-        mwebView = (WebView) findViewById(R.id.webview);
-        mwebView.setWebChromeClient(new WebChromeClient());
-        mwebView.loadUrl("https://yangseungwon.github.io/IchVoting/");
+
+        setLayout();
+
+        // 웹뷰에서 자바스크립트실행가능
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setLoadsImagesAutomatically(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setSupportZoom(false);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        mWebView.getSettings().setAppCacheEnabled(false);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setAllowFileAccess(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+
+
+        // 구글홈페이지 지정
+        mWebView.loadUrl("https://yangseungwon.github.io/IchVoting/");
+        // WebViewClient 지정
+        mWebView.setWebViewClient(new WebViewClientClass() {
+            @Override
+            @JavascriptInterface
+            public void onPageFinished(WebView view, String url){
+                if(url.equals("https://yangseungwon.github.io/IchVoting/")){
+                    String keyword = "abcdefd";
+                    String script = "javascript:function dataset() {"
+                            + "document.getElementById('privatekey').value = '" + keyword + "';"
+                            + "};"
+                            + "dataset();";
+
+                    mWebView.loadUrl(script);
+                }
+            }
+        });
+    }
+
+    public void clicked() {
+
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    /*
+     * Layout
+     */
+    private void setLayout(){
+        mWebView = (WebView) findViewById(R.id.webview);
     }
 }
