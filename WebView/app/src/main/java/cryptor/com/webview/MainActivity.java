e86dbde95feef622c9013e9e1d382a111eec738d
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2018-06-14 01:40:35 +0900

    완료? zxing으로 앱에서 QR코드 인식
    http://dwfox.tistory.com/79

--- WebView/app/src/main/java/cryptor/com/webview/MainActivity.java
+++ WebView/app/src/main/java/cryptor/com/webview/MainActivity.java
@@ -1,24 +1,34 @@
 package cryptor.com.webview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends Activity{
 
     private WebView mWebView;
+    String keyword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//실행되면
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         setLayout();
+
+        new IntentIntegrator(this).initiateScan(); //qr코드 인식 바로 실행
 
         // 웹뷰에서 자바스크립트실행가능
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -33,8 +43,6 @@ public class MainActivity extends Activity{
         mWebView.getSettings().setAllowFileAccess(true);
         mWebView.setWebChromeClient(new WebChromeClient());
 
-
-        // 구글홈페이지 지정
         mWebView.loadUrl("https://yangseungwon.github.io/IchVoting/");
         // WebViewClient 지정
         mWebView.setWebViewClient(new WebViewClientClass() {
@@ -42,7 +50,6 @@ public class MainActivity extends Activity{
             @JavascriptInterface
             public void onPageFinished(WebView view, String url){
                 if(url.equals("https://yangseungwon.github.io/IchVoting/")){
-                    String keyword = "abcdefd";
                     String script = "javascript:function dataset() {"
                             + "document.getElementById('privatekey').value = '" + keyword + "';"
                             + "};"
@@ -54,16 +61,26 @@ public class MainActivity extends Activity{
         });
     }
 
-    public void clicked() {
-
-    }
-
     private class WebViewClientClass extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        String QR_code = result.getContents();
+
+        if(QR_code == null){
+            Toast.makeText(this,"404 NOT FOUND",Toast.LENGTH_SHORT).show();
+        }else{
+            keyword=QR_code;
+            mWebView.loadUrl("https://yangseungwon.github.io/IchVoting/");
+        }
+        super.onActivityResult(requestCode,resultCode,data);
     }
 
     /*
